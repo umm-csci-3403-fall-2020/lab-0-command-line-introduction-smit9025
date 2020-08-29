f6a11cccb7c2ba5e7c71da888ed6d717f74b1ddd
root <root@FriggOff.localdomain>
2020-08-29 18:16:23 -0500

    Finish extract_and_compile.sh Finished adding script to the extract_and_compile.sh file and tested the bat file to make sure all tests passed. All tests passed

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
-
-tree
+tar xzf NthPrime.tgz #This line will extract the tgz file as well as uncompress the file. x = extract, v = lists files extracted, z = uncompress, f = file name
+cd NthPrime #Here we go into the directory that is extracted
+gcc main.c nth_prime.c nth_prime.h -o NthPrime #This line compiles the program and names it
+./NthPrime $1 #This line runs the compiled program with the argument that was inputted with the bash file
