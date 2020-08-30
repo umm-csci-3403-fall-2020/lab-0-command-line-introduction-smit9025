967a226f8a1b19308d8da07c115292253a0f18f9
root <root@FriggOff.localdomain>
2020-08-30 00:18:51 -0500

    Shellcheck both new script files and make changes to them Shellchecked both big_clean.sh and extract_and_compile.sh to make sure that they checked out okay, then made changes within the files so that nothing comes up with shellcheck.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,11 +2,12 @@
 FILE=$1 #Store name of tgz file inputted
 file="${FILE//.tgz}" #Take stored name of inputted file and remove .tgz from the name
 here=$(pwd) #Stores current path location
-tmpDir=`mktemp -d` && tar -C $tmpDir -xvf $1 #Creates a temp directory and extracts the inputted tar file into the temp directory
+tmpDir=$(mktemp -d) 
+tar -C "$tmpDir" -xf "$1" #Creates a temp directory and extracts the inputted tar file into the temp directory
 cd / #Moves current location to the root
-cd $tmpDir/$file #Moves current location into the temp directory
+cd "$tmpDir"/"$file" || exit #Moves current location into the temp directory
 grep -l --null DELETE ./* | xargs -0 rm #Looks at every file that is in the directory and deletes files that have the word DELETE in them
 cd .. #Moves current location back one
-tar -czf cleaned_$FILE $file #Compresses files in inputted file name directory that holds the extracted files into a "cleaned" tar archive
+tar -czf cleaned_"$FILE" "$file" #Compresses files in inputted file name directory that holds the extracted files into a "cleaned" tar archive
 cd / #Moves current location back to root
-mv $tmpDir/cleaned_$FILE $here #Moves the new tar archive in the temp directory over to the cleaning directory in the lab
+mv "$tmpDir"/cleaned_"$FILE" "$here" #Moves the new tar archive in the temp directory over to the cleaning directory in the lab
--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 tar xzf NthPrime.tgz #This line will extract the tgz file as well as uncompress the file. x = extract, v = lists files extracted, z = uncompress, f = file name
-cd NthPrime #Here we go into the directory that is extracted
+cd NthPrime || exit #Here we go into the directory that is extracted
 gcc main.c nth_prime.c nth_prime.h -o NthPrime #This line compiles the program and names it
-./NthPrime $1 #This line runs the compiled program with the argument that was inputted with the bash file
+./NthPrime "$1" #This line runs the compiled program with the argument that was inputted with the bash file
